7e373906333173c118fac3182e7ba483554e4319
Sumit Jethani <sjethani651@gmail.com>
2026-09-19 21:03:45 +0530

    project setup and DB connection

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(`\n Mongo DB Connected !! DB HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("Mongo DB connection error", error);
+    process.exit(1);
+  }
+};
+
+export { connectDB };
